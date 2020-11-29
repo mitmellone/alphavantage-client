b17f0153a9a4fc499591f6e0c5da9912485cd4db
mitmellone <mitmellone>
2020-11-29 02:09:20 +0000

    Initial commit

---
+++ src/test/groovy/alphavantage/AlphavantageClientSpec.groovy
@@ -0,0 +1,19 @@
+package alphavantage
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import javax.inject.Inject
+
+@MicronautTest
+class AlphavantageClientSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
